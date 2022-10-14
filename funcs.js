f4c965b5bf36bbace01ce43e4756eb88d44574ce
abraham <abrahamgitonga98@gmail.com>
2022-10-14 12:11:44 +0300

    initialize decreament fizzbuzz

--- funcs.js
+++ funcs.js
@@ -19,3 +19,22 @@ function rec(num){
 rec(1)
 
 //from 100 to 1
+
+function rec(num){
+    if(num>=1){
+        if(num % 3=== 0 && num % 5===0){
+            console.log("fizzbuzz")
+        }
+        else if(num %3 ===0){
+            console.log("fizz")
+        }
+        else if(num % 5===0){
+            console.log("buzz")
+        }
+        else{
+            console.log(num);
+        }
+        rec(num-1)
+    }
+}
+rec(100)
